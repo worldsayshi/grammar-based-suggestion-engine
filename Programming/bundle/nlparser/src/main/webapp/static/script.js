f1e445d52d2cb769417b4d025d69945be361f69a
Marcin Goss <marcin.goss@findwise.com>
2015-02-20 12:19:03 +0100

    basic speech recognition in place

--- Programming/bundle/nlparser/src/main/webapp/static/script.js
+++ Programming/bundle/nlparser/src/main/webapp/static/script.js
@@ -58,121 +58,53 @@ $(function () {
 
 //Speech recognition stuff ------------------------------------------------------------
 
-showInfo('info_start');
-
-var final_transcript = '';
 var recognizing = false;
-var ignore_onend;
-var start_timestamp;
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'en-US';
+recognition.continuous = true;
+recognition.interimResults = true;
 
-if (!('webkitSpeechRecognition' in window)) {
-    upgrade();
-} else {
-    start_button.style.display = 'inline-block';
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.onstart = function() {
-        recognizing = true;
-        showInfo('info_speak_now');
-        start_img.src = 'static/mic-animate.gif';
-    };
-    recognition.onerror = function(event) {
-        if (event.error == 'no-speech') {
-            start_img.src = 'static/mic.gif';
-            showInfo('info_no_speech');
-            ignore_onend = true;
-        }
-        if (event.error == 'audio-capture') {
-            start_img.src = 'static/mic.gif';
-            showInfo('info_no_microphone');
-            ignore_onend = true;
-        }
-        if (event.error == 'not-allowed') {
-            if (event.timeStamp - start_timestamp < 100) {
-                showInfo('info_blocked');
-            } else {
-                showInfo('info_denied');
-            }
-            ignore_onend = true;
-        }
-    };
-    recognition.onend = function() {
-        recognizing = false;
-        if (ignore_onend) {
-            return;
-        }
-        start_img.src = 'static/mic.gif';
-        if (!final_transcript) {
-            showInfo('info_start');
-            return;
-        }
-        showInfo('');
-        if (window.getSelection) {
-            window.getSelection().removeAllRanges();
-            var range = document.createRange();
-            range.selectNode(document.getElementById('final_span'));
-            window.getSelection().addRange(range);
-        }
-    };
-    recognition.onresult = function(event) {
-        var interim_transcript = '';
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            if (event.results[i].isFinal) {
-                final_transcript += event.results[i][0].transcript;
-            } else {
-                interim_transcript += event.results[i][0].transcript;
-            }
-        }
-        final_transcript = capitalize(final_transcript);
-        final_span.innerHTML = linebreak(final_transcript);
-        interim_span.innerHTML = linebreak(interim_transcript);
-    };
-}
-function upgrade() {
-    start_button.style.visibility = 'hidden';
-    showInfo('info_upgrade');
-}
+var final_text = "";
+var interim_text = "";
 
-var two_line = /\n\n/g;
-var one_line = /\n/g;
-function linebreak(s) {
-return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-}
-var first_char = /\S/;
-function capitalize(s) {
-return s.replace(first_char, function(m) { return m.toUpperCase(); });
-}
+recognition.onresult = function(event) {
+      
+    interim_text = "";
+    
+    for( var i = event.resultIndex; i < event.results.length; ++i){
+        q.value += event.results[i][0].transcript;
+    }
+    
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+            final_text += event.results[i][0].transcript;
+        } else {
+            interim_text += event.results[i][0].transcript;
+        }
+    }
+    
+    q.value = final_text + interim_text;
+};
 
-function startButton(event) {
+recognition.onstart = function() {
+    recognizing = true;
+    speakButton.value = "Click to Stop!";
+};
+
+recognition.onend = function() {
+    recognizing = false;
+    speakButton.value = "Click to Speak!";
+};
+    
+function startButton() {
     if (recognizing) {
         recognition.stop();
         return;
     }
-    final_transcript = '';
-    recognition.lang = 'en-US';
+    q.value = "";
+    interim_text = "";
+    final_text = "";
+
     recognition.start();
-    ignore_onend = false;
-    final_span.innerHTML = '';
-    interim_span.innerHTML = '';
-    start_img.src = 'static/mic-slash.gif';
-    showInfo('info_allow');
-    //showButtons('none');
-    start_timestamp = event.timeStamp;
-}
-
-var info = document.getElementById('info');
-
-function showInfo(s) {
-    if (s) {
-        for (var child = document.getElementById('info').firstChild; child; child = child.nextSibling) {
-            if (child.style) {
-                child.style.display = child.id == s ? 'inline' : 'none';
-            }
-        }
-        document.getElementById('info').style.visibility = 'visible';
-    } else {
-        document.getElementById('info').style.visibility = 'hidden';
-    }
-}
+};    
 
